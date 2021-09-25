2ebe1f6675f3aa1acb37fe995e457addfd121eff
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-25 17:51:26 +0300

    configuring heroku for deployment

--- app/config.py
+++ app/config.py
@@ -28,6 +28,12 @@ class ProdConfig(Config):
         Config: The parent configuration class with Generl configuration settings
     """
 
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1
+        )
+
 
 class DevConfig(Config):
     """
